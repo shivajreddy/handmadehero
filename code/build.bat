89b69cf22b76fd2d2f22380990526e73abb76f15
shiva <shivajreddy@outlook.com>
2025-02-01 16:16:04 -0500

    windows build setup and nvim setup

--- code/build.bat
+++ code/build.bat
@@ -1,12 +1,21 @@
+@echo off
 
-REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+:: Set up the Visual Studio environment (this takes like 1-2 seconds :/)
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
+:: Create and go to build directory
 mkdir ..\build
 pushd ..\build
 
-pwd
-cl -Zi ..\code\win32_handmade.cpp
+:: Compile the code
+echo Compiling...
+cl -Zi ..\code\win32_handmade.cpp user32.lib
+
+:: Check if compilation succeeded before running
+if exist win32_handmade.exe (
+    echo Running the executable...
+    win32_handmade.exe
+) else (
+    echo Compilation failed. Exe not found.
+)
 popd
-
-pushd ..\code
-
