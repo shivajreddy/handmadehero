e58eeaa7ca5dc3627ca951423496029047ca6576
shiva <shivajreddy@outlook.com>
2025-02-01 01:08:51 -0500

    basic build script

--- code/build.bat
+++ code/build.bat
@@ -1,4 +1,12 @@
 
-mkdir ..\..\build
-pushd ..\..\build
+REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+mkdir ..\build
+pushd ..\build
+
+pushd ..\code
+
 pwd
+
+cl ..\code\win32_handmade.cpp
+
